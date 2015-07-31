44a42a1d19b9e97361625fdb7b90b27f3fa2a714
Seth Sanchez <theoneghost@gmail.com>
2015-07-30 19:08:13 -0500

    spelling_fixes, updated copyright

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ObjectRocket'
-copyright = u'2014, ObjectRocket'
+copyright = u'2015, ObjectRocket'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
